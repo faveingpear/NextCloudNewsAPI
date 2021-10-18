7fd0cf984358d33dda96dfc59eaf45350d9368e1
faveing <faveing@pop-os.localdomain>
2021-10-18 12:15:52 -0700

    modified:   api.py

--- api.py
+++ api.py
@@ -11,12 +11,23 @@ class NextcloudNewsApi:
     baseurl = None
     header = None
 
-    def __init__(self,appName, nextcloudUrl=None, username=None, password=None) -> None:
+    logger = None
 
-        self.appName= appName
+    def __init__(self,appName, nextcloudUrl=None, username=None, password=None, logger=None) -> None:
+
+        # if logger == None:
+        #     logging.basicConfig(filename="newsApi.log", format = '%(asctime)s %(message)s', datefmt = '%m/%d/%Y %I:%M:%S %p',force=True)
+        #     self.logger = logging.getLogger()
+        #     print("ran")
+        # else:
+        #     self.logger = logger
+
+        #self.logger = logging.getLogger()
+
+        self.appName = appName
 
         if  nextcloudUrl == None and username == None and password == None:
-            logging.info(self.appName + ": No url,username,password inputed, skipping authentication")
+            logging.info(self.appName + "No url,username,password inputed, skipping authentication")
             pass
         else:
             self.auth(nextcloudUrl=nextcloudUrl, username=username, password=password)
@@ -31,8 +42,32 @@ class NextcloudNewsApi:
             "Authorization": "Basic " + base64.b64encode(bytes(auth, "utf-8")).decode()
         }
 
-        logging.info("NewsApi: Generated auth tokens")
+        logging.info(self.appName + ": Generated auth tokens")
+
+        try:
+            self.getFolders()
+        except Exception:
+            return False
+        
+        return True
+
+    def getFolders(self):
+        route = "folders"
+
+        params = {
+
+        }
     
+        r = self.get(route=route, params=params)
+
+        #self.logger.info("Route:" + route + " \n Status code:" + str(r.status_code))
+
+        logging.info(self.appName +": Route:" + route + " \n Status code:" + str(r.status_code))
+
+        self.errorHandeler(status_code=r.status_code)
+
+        return r.json()
+
     def getUnread(self,batchSize=-1,offset=None,type=3,id=None,getRead='false',oldestFirst='false'):
         route = "items"
 
@@ -48,6 +83,9 @@ class NextcloudNewsApi:
         r = self.get(route=route, params=params)
 
         logging.info("Route:" + route + " \n Status code:" + str(r.status_code))
+
+        self.errorHandeler(status_code=r.status_code)
+
         print(r.status_code)
 
         return r
@@ -58,10 +96,32 @@ class NextcloudNewsApi:
 
         return requests.get(url=self.baseurl + route, headers=self.header, verify=False, params=params)
 
+    def errorHandeler(self, status_code):
+        if status_code == 404:
+            logging.info(self.appName + ": Unable to connect url=" + self.baseurl)
+            raise Exception("Unable to connect")
+        
+        if status_code == 401:
+            logging.info(self.appName + ": Username or password is incoreect")
+            raise Exception("Username or password is incorrect")
 
 def main():
-    newsApi = NextcloudNewsApi()
-    newsApi.auth(nextcloudUrl="https://nextcloud.tail", username="ncp", password="459799OoMatt")
+    logging.basicConfig(filename="newsApi.log",filemode="a", format = '%(asctime)s %(message)s', datefmt = '%m/%d/%Y %I:%M:%S %p',force=True)
+    newsApi = NextcloudNewsApi(appName="News")
+
+    login = False
+
+    while login == False:
+        url = input("Enter url: ")
+        username = input("Enter username: ")
+        password = input("Enter Password")
+
+        if newsApi.auth(nextcloudUrl=url, username=username, password=password):
+            login = True
+        else:
+            print("Username or password is incorrect")
+            
+        
     print(newsApi.header)
 
     unread = newsApi.getUnread().json()
@@ -73,6 +133,13 @@ def main():
         print("Read:" + str(item['unread']))
         print("Feedid:" + str(item['feedId']))
         print("-"*50)
+
+    folders = newsApi.getFolders()
+
+    for item in folders['folders']:
+        print("-"*50)
+        print("id: " + str(item['id']))
+        print("name: " + str(item['name']))
 
 if __name__ == "__main__":
     main()
